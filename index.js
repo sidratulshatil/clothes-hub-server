97db1f2ef07ab4fed037b6e79b2dd5ebf012946e
sidratulshatil <105915771+sidratulshatil@users.noreply.github.com>
2022-11-24 11:32:20 +0600

    category added

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const cors = require('cors')
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
+require('dotenv').config()
 const app = express()
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwh8ees.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    const clothesCategoryCollection = client.db('clothesHub').collection('clothesCategory')
+
+    app.get('/category', async (req, res) => {
+        const query = {}
+        const result = await clothesCategoryCollection.find(query).toArray()
+        res.send(result)
+    })
+}
+run().catch(console.log)
+
 
 app.get('/', async (req, res) => {
     res.send('Clothes Hub Server Running')
