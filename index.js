10e3a873d2268e8b4a7deeb4bb548ddc4ee5771d
sidratulshatil <105915771+sidratulshatil@users.noreply.github.com>
2022-11-26 00:03:07 +0600

    Handle delete add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config()
 const app = express()
@@ -100,6 +100,12 @@ async function run() {
         const query = { email }
         const user = await usersCollection.findOne(query)
         res.send({ isBuyers: user?.type === 'Buyers' })
+    })
+    app.delete('/users/:id', async (req, res) => {
+        const id = req.params.id
+        const filter = { _id: ObjectId(id) }
+        const result = await usersCollection.deleteOne(filter)
+        res.send(result)
     })
     app.post('/bookings', async (req, res) => {
         const booking = req.body
